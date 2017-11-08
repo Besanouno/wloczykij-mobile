f94b9d71ba4eea17da7ea95771aca4559cd69814
Marcin <basistamarcin@gmail.com>
2017-11-08 11:39:04 +0100

    Added account managing

--- app/src/main/java/pl/basistam/turysta/MainActivity.java
+++ app/src/main/java/pl/basistam/turysta/MainActivity.java
@@ -4,7 +4,9 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.accounts.AccountManagerCallback;
 import android.accounts.AccountManagerFuture;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,9 +19,12 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import pl.basistam.turysta.auth.AccountGeneral;
 import pl.basistam.turysta.components.utils.KeyboardUtils;
@@ -106,44 +111,76 @@ public class MainActivity extends AppCompatActivity
             searchPanel.setVisibility(View.VISIBLE);
         } else if (id == R.id.nav_event) {
             fragmentManager.beginTransaction().replace(R.id.content, eventFragment).commit();
-        } else if (id == R.id.nav_log_in) {
-            AccountManager accountManager = AccountManager.get(this);
-            accountManager.addAccount(AccountGeneral.ACCOUNT_TYPE, AccountGeneral.AUTHTOKEN_TYPE_FULL_ACCESS, null, null, this, new AccountManagerCallback<Bundle>() {
-                @Override
-                public void run(AccountManagerFuture<Bundle> future) {
-                    try {
-                        future.getResult();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }, null);
-        } else if (id == R.id.nav_sign_up) {
-            Intent signup = new Intent(this, SignUpActivity.class);
-            signup.putExtra(ARG_ACCOUNT_TYPE, AccountGeneral.AUTHTOKEN_TYPE_FULL_ACCESS);
-            startActivity(signup);
+        } else if (id == R.id.nav_manage_account) {
+            showAccountPicker(AccountGeneral.AUTHTOKEN_TYPE_FULL_ACCESS);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
     private void checkIfLoggedIn() {
-        AccountManager accountManager = AccountManager.get(this);
+        /*AccountManager accountManager = AccountManager.get(this);
         Account[] accounts = accountManager.getAccountsByType(AccountGeneral.ACCOUNT_TYPE);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        if (accounts.length > 0) {
-            MenuItem item = navigationView.getMenu().findItem(R.id.account).getSubMenu().findItem(R.id.nav_unlog);
-           /* navigationView.getMenu().getItem((R.id.nav_log_in)).setVisible(false);
-            navigationView.getMenu().getItem((R.id.nav_sign_up)).setVisible(false);
-            navigationView.getMenu().getItem((R.id.nav_unlog)).setVisible(true);*/
-        } else {
-          /*  navigationView.getMenu().getItem((R.id.nav_log_in)).setVisible(true);
-            navigationView.getMenu().getItem((R.id.nav_sign_up)).setVisible(true);
-            navigationView.getMenu().getItem((R.id.nav_unlog)).setVisible(false);*/
-        }
-        navigationView.invalidate();
+
+        navigationView.invalidate();*/
     }
 
+    private void showAccountPicker(final String authTokenType) {
+        final Account availableAccounts[] = AccountManager.get(this).getAccountsByType(AccountGeneral.ACCOUNT_TYPE);
+
+        final int itemsNumber = availableAccounts.length + 1;
+        final String items[] = new String[itemsNumber];
+        for (int i = 0; i < availableAccounts.length; i++) {
+            items[i] = availableAccounts[i].name;
+        }
+        items[itemsNumber - 1] = "Dodaj konto";
+        // Account picker
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("Wybierz konto")
+                .setAdapter(new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_list_item_1, items), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == itemsNumber - 1) {
+                            signIn();
+                        } else {
+                            getExistingAccountAuthToken(availableAccounts[which], authTokenType);
+                        }
+                    }
+                }).create();
+        alertDialog.show();
+    }
+
+    private void getExistingAccountAuthToken(Account account, String authTokenType) {
+        final AccountManagerFuture<Bundle> future = AccountManager
+                .get(this)
+                .getAuthToken(account, authTokenType, null, this, null, null);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Bundle bnd = future.getResult();
+                    final String authtoken = bnd.getString(AccountManager.KEY_AUTHTOKEN);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void signIn() {
+        AccountManager accountManager = AccountManager.get(this);
+        accountManager.addAccount(AccountGeneral.ACCOUNT_TYPE, AccountGeneral.AUTHTOKEN_TYPE_FULL_ACCESS, null, null, this, new AccountManagerCallback<Bundle>() {
+            @Override
+            public void run(AccountManagerFuture<Bundle> future) {
+                try {
+                    future.getResult();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, null);
+    }
 }
